4b172af0f33dec80550d0ad4fcc80547cc30c8c5
Zeeshan-Wassma <Zeeshan@ibsksa.com>
2023-03-20 13:19:41 +0530

    Update common.js

--- common.js
+++ common.js
@@ -19,4 +19,13 @@ const zeroPad = (num, places) => String(num).padStart(places, '0');
       })
    }
 
+   function generate_branch_no (com_id) {
+    let q = `select count(*) from company_branches where com_id = '${com_id}'`;
+    return new Promise((resolve, reject) => {
+        pool.query(q, (err, res) => {
+          err ? reject(err) : parseInt(res.rows[0].count) > 0 ? resolve(zeroPad(parseInt(res.rows[0].count) +1, 3)): resolve("001");
+        })
+      })
+   }
+
 module.exports = { generate_emp_no, generate_comp_no }
