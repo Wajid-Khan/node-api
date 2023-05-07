23ad18dab158e902a95daf7e7b597bce6ebce3b1
Wajid Khan <wajid.developer1@gmail.com>
2023-05-07 15:31:17 +0530

    Update db.js

--- db.js
+++ db.js
@@ -3,7 +3,7 @@ const Pool = require("pg").Pool;
 const pool = new Pool({
     user : "postgres",
     password: 'IBSks@123$',
-    host : "65.2.130.99",
+    host : "13.126.161.113",
     port : 5432,
     database : "af_db_new"
 });
